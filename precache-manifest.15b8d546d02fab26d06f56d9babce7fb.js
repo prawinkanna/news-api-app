debcda8e572e9277e237b6dd0de2ac9424420197
prawinkanna <45977611+prawinkanna@users.noreply.github.com>
2020-10-16 11:01:20 +0530

    updated

---
+++ precache-manifest.15b8d546d02fab26d06f56d9babce7fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c27609d0e34558b72d50d6814adf0c",
+    "url": "/news-api-app/index.html"
+  },
+  {
+    "revision": "3a7361d466da43e678e6",
+    "url": "/news-api-app/static/css/main.d9a9dddb.chunk.css"
+  },
+  {
+    "revision": "48983b3546bfe1588b62",
+    "url": "/news-api-app/static/js/2.b94cebc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-api-app/static/js/2.b94cebc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7361d466da43e678e6",
+    "url": "/news-api-app/static/js/main.78bc578c.chunk.js"
+  },
+  {
+    "revision": "a53a2bbedc5693817fc1",
+    "url": "/news-api-app/static/js/runtime-main.f9f4559b.js"
+  },
+  {
+    "revision": "506d53b87b3f8ccf82d924307a9aa4d3",
+    "url": "/news-api-app/static/media/banner1.506d53b8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.136cda3ca3bbb7fa19d87dab19115043.js"
+  "/news-api-app/precache-manifest.15b8d546d02fab26d06f56d9babce7fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/news-api-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
